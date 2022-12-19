4ce4a27015084557988891f093d6935b4009690b
huynhit24 <huynh.it.24@gmail.com>
2022-12-19 23:28:22 +0700

    run successfull but still errors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 mongoose.Promise = global.Promise;
 
-mongoose.connect('')
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
 
